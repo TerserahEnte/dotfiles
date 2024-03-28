41d399993bf5bc1623188f5a382ab3aff20d96a3
TerserahEnte <86144698+TerserahEnte@users.noreply.github.com>
2024-03-28 18:42:46 +0700

    Add files via upload

---
+++ .config/waybar/custom/lenovo_conservation_check.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Customize this with the path to the file where the value is stored
+value_file="/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
+
+# Read the current value from the file
+current_value=$(cat "$value_file")
+
+# Toggle the value
+if [[ "$current_value" -ne 1 ]]; then
+    new_value=
+    tooltip="Conservation Mode Off"
+else
+    new_value=
+    tooltip="Conservation Mode On"
+fi
+
+# Write the new value back to the file
+echo "{\"text\":\"$new_value\", \"tooltip\":\"$tooltip\"}"
